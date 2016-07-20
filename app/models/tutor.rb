d57721a07a8821e2fc5088803d5c65af8e105b02
Luis Gaytan <gaytan_luis@msn.com>
2016-07-20 01:28:06 -0700

    Devise configured with Tutor model

---
+++ app/models/tutor.rb
@@ -0,0 +1,6 @@
+class Tutor < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :tutors
   root to: 'welcome#index'
   get 'welcome/contact'
 
