3d7e26762444aebe75f3035b4bf4d77b875f1a1e
Luis Gaytan <gaytan_luis@msn.com>
2016-07-23 01:23:08 -0700

    controller index and show

--- app/controllers/tutoring_sessions_controller.rb
+++ app/controllers/tutoring_sessions_controller.rb
@@ -2,9 +2,11 @@ class TutoringSessionsController < ApplicationController
   before_action :authenticate_tutor!
 
   def index
+    @sessions = TutoringSession.all
   end
 
   def show
+    @session = TutoringSession.find(params[:id])
   end
 
   def new
