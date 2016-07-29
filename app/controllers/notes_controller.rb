d11131ce4d8f74ea56588b886c6ede242ea94081
Luis Gaytan <gaytan_luis@msn.com>
2016-07-29 01:02:25 -0700

    devise layouts changed. Notes New started.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,18 @@ class NotesController < ApplicationController
 
   def new
     @note = Note.new
+    @session = TutoringSession.find(params[:tutoring_session_id])
+  end
+
+  def create
+    @note = Note.new(params.require(:note).permit(:summary, :achievement, :what_next, :recommendation, :rate, :date_session, :tm_start, :tm_end, :tutoring_session_id))
+    if @note.save
+      flash[:notice] = "Note was created sucessfully."
+      redirect_to @note
+    else
+      flash[:error] = "There was an issue creating the note. Please try again."
+      render :new
+    end
   end
 
   def show
