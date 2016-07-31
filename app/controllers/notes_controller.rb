ff198d5c5cece91171581b6ca9b6ba952517adfc
Luis Gaytan <gaytan_luis@msn.com>
2016-07-31 01:36:13 -0700

    edit for notes working

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,8 +19,21 @@ class NotesController < ApplicationController
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update_attributes(params.require(:note).permit(:summary, :achievement, :what_next, :recommendation, :rate, :date_session, :tm_start, :tm_end, :tutoring_session_id))
+      flash[:notice] = "Note was updated sucessfully."
+      redirect_to @note
+    else
+      flash[:error] = "There was an issue updating the note. Please try again."
+      render :new
+    end
   end
 end
